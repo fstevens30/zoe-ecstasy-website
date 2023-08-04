486559400a88b9376d51a4058a10b589615d3972
Flynn Stevens <flynnstevens30@gmail.com>
2023-08-04 15:21:28 +1200

    Added basic spotify widget

--- zoe-ecstasy-website/src/components/listen/listen.js
+++ zoe-ecstasy-website/src/components/listen/listen.js
@@ -2,7 +2,7 @@ import './listen.css';
 
 function Listen() {
     return (
-        <p>Listen</p>
+        <iframe src="https://open.spotify.com/embed/track/3AULWj8ousj9N9YU4shERR?utm_source=generator&theme=0" width="100%" height="352" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     );
 }
 
