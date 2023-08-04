b08442786e307fb080e98e153fddd84b076cfd69
Flynn Stevens <flynnstevens30@gmail.com>
2023-08-04 15:23:26 +1200

    Created a congs component for displaying all songs

--- zoe-ecstasy-website/src/App.js
+++ zoe-ecstasy-website/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/header/header.js';
+import Social from './components/social-links/social.js';
+import Listen from './components/listen/listen.js';
+import Songs from './components/songs/songs.js';
+import Footer from './components/footer/footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Social />
+      <Listen />
+      <Songs />
+      <Footer />
     </div>
   );
 }
---
+++ zoe-ecstasy-website/src/components/songs/songs.js
@@ -0,0 +1,10 @@
+import './songs.css';
+import './songs.json';
+
+function Songs() {
+    return (
+        <p>Songs</p>
+    );
+}
+
+export default Songs;
