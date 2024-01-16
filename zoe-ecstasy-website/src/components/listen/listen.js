ed0ce2177606828f848a14a9d3bc18e2adf910a8
Flynn Stevens <flynnstevens30@gmail.com>
2024-01-16 15:49:18 +1300

    Create site folder

--- zoe-ecstasy-website/src/components/listen/listen.js
+++ zoe-ecstasy-website/src/components/listen/listen.js
@@ -2,7 +2,7 @@ import './listen.css';
 
 function Listen() {
     return (
-        <iframe src="https://open.spotify.com/embed/track/3AULWj8ousj9N9YU4shERR?utm_source=generator&theme=0" width="100%" height="352" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe src="https://open.spotify.com/embed/track/3AULWj8ousj9N9YU4shERR?utm_source=generator&theme=0" width="100%" height="auto" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     );
 }
 
